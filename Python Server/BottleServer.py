fc7c8e8fe0c4ffe0eaa77a909b20b2ee70e2a504
Tyler Yeats <tyeats@uw.edu>
2016-04-26 23:24:16 -0700

    Added servo get value

--- Python Server/BottleServer.py
+++ Python Server/BottleServer.py
@@ -6,7 +6,9 @@ import Adafruit_PCA9685
 # Initialize the PCA9685 using the default address (0x40)
 pwm = Adafruit_PCA9685.PCA9685()
 
-sensor_values = "hngdhgf"
+sensor_values = "{}"
+
+servo_values = {}
 
 # Set frequency to 60hz
 pwm.set_pwm_freq(60)
@@ -22,7 +24,15 @@ def set_sensor(value):
 
 @get('/servo/<channel>/<value>')
 def set_servo(channel, value):
-    pwm.set_pwm(channel, 0, value)
-    #return "Running channel {0} with value {1}".format(channel, value)
+    servo_values[channel] = value
+    # pwm.set_pwm(channel, 0, value)
+    return "Running channel {0} with value {1}".format(channel, value)
+
+@get('/servo/<channel>')
+def get_servo(channel):
+    if channel in servo_values:
+        return str(servo_values[channel])
+    else:
+        return "0"
 
 run(host='localhost', port=8085)
