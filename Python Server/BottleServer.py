6a0e405e7bcaa1d3d579ebff529a08c783a30aeb
Tyler Yeats <tyeats@uw.edu>
2016-04-28 17:31:32 -0700

    Added servo orientation conversion

--- Python Server/BottleServer.py
+++ Python Server/BottleServer.py
@@ -1,17 +1,25 @@
 from bottle import get, put, route, run, template
 
 # Import the PCA9685 module
-import Adafruit_PCA9685
+#import Adafruit_PCA9685
 
 # Initialize the PCA9685 using the default address (0x40)
-pwm = Adafruit_PCA9685.PCA9685()
+#pwm = Adafruit_PCA9685.PCA9685()
 
 sensor_values = "{}"
 
 servo_values = {}
 
 # Set frequency to 60hz
-pwm.set_pwm_freq(60)
+#pwm.set_pwm_freq(60)
+
+def test_transform(value):
+    return "look at me! I work!" + value
+
+#Dict of Servo value transformation functions
+value_transforms = {}
+value_transforms[1] = lambda x: str(int(x) * 500)
+
 
 @get('/sensor')
 def getsensor():
@@ -24,9 +32,10 @@ def set_sensor(value):
 
 @get('/servo/<channel>/<value>')
 def set_servo(channel, value):
-    servo_values[channel] = value
-    # pwm.set_pwm(channel, 0, value)
-    return "Running channel {0} with value {1}".format(channel, value)
+    return value_transforms.get(int(channel), lambda x: x)(value)
+    #servo_values[channel] = value
+    #pwm.set_pwm(channel, 0, value)
+    #return "Running channel {0} with value {1}".format(channel, value)
 
 @get('/servo/<channel>')
 def get_servo(channel):
